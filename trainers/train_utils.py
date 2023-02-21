19ef9d26f2d57ff68e3cdd8dfe9eb1d99480cc7e
Avii Ahuja <avii.ahuja@gmail.com>
2023-02-20 23:24:59 -0800

    complete milestone 1

--- trainers/train_utils.py
+++ trainers/train_utils.py
@@ -19,38 +19,42 @@ from transformers import (
     AutoTokenizer,
 )
 
-def evaluate_standard(preds, labels, scoring_method):
+from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score
 
+
+def evaluate_standard(preds, labels, scoring_method):
     # The accuracy, precision, recall and F1 scores to return
     acc, prec, recall, f1 = 0.0, 0.0, 0.0, 0.0
 
     ########################################################
-    # TODO: Please finish the standard evaluation metrics.
     # You need to compute the accuracy, precision, recall
     # and F1 score for the predictions and gold labels.
     # Please also make your sci-kit learn scores are computed
     # using `scoring_method` for the `average` argument.
-    raise NotImplementedError("Please finish the TODO!")
-    # End of TODO
+    acc = accuracy_score(labels, preds)
+    prec = precision_score(labels, preds, average=scoring_method)
+    recall = recall_score(labels, preds, average=scoring_method)
+    f1 = f1_score(labels, preds, average=scoring_method)
     ########################################################
 
     return acc, prec, recall, f1
 
-def pairwise_accuracy(guids, preds, labels):
 
+def pairwise_accuracy(guids, preds, labels):
     acc = 0.0  # The accuracy to return.
-    
+
     ########################################################
-    # TODO: Please finish the pairwise accuracy computation.
     # Hint: Utilize the `guid` as the `guid` for each
     # statement coming from the same complementary
     # pair is identical. You can simply pair the these
     # predictions and labels w.r.t the `guid`. 
-    raise NotImplementedError("Please finish the TODO!")
-    # End of TODO
+    y_true = labels[guids]
+    y_pred = preds[guids]
+    acc = accuracy_score(y_true, y_pred)
     ########################################################
-     
+
     return acc
+
 
 if __name__ == "__main__":
 
@@ -61,7 +65,7 @@ if __name__ == "__main__":
     acc, prec, rec, f1 = evaluate_standard(preds, labels, "binary")
     pair_acc = pairwise_accuracy(guids, preds, labels)
 
-    if acc == 0.75 and prec == 1.0 and round(rec,2) == 0.67 and f1 == 0.8:
+    if acc == 0.75 and prec == 1.0 and round(rec, 2) == 0.67 and f1 == 0.8:
         print("Your `evaluate_standard` function is correct!")
     else:
         raise NotImplementedError("Your `evaluate_standard` function is INCORRECT!")
